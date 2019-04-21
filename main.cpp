431d34f5131d09bd2ce7d2eb84364786ef135a51
andierodin@gmail.com <andierodin@gmail.com>
2019-04-21 14:22:56 +0300

    Realised Pattern FactoryMethod for strategy Game

--- main.cpp
+++ main.cpp
@@ -1,9 +1,65 @@
 #include <iostream>
-
+#include<vector>
 using namespace std;
+class Warrior {
+public:
+  virtual void Info()=0;
+  virtual ~Warrior (){}
+};
+
+class InfantryMan:public Warrior{
+public:
+  void Info(){
+    cout<<"Infantryman"<<endl;
+  }
+};
+class Archer:public Warrior{
+public:
+  void Info(){
+    cout<<"Archer"<<endl;
+  }
+};
+class HorseMan:public Warrior{
+public:
+  void Info(){
+    cout<<"Horseman"<<endl;
+  }
+};
+
+class Factory
+{
+public:
+  virtual Warrior* createWarrior()=0;
+  virtual ~Factory()=default;
+};
+class InfantryManFactory :public Factory{
+public:
+   Warrior* createWarrior() {
+    return new InfantryMan;
+  }
+};
+
+class ArcherManFactory:public Factory{
+public:
+  Warrior *createWarrior(){
+    return new Archer;
+  }
+};
+class HorseManFactory:public Factory {
+public:  Warrior *createWarrior(){
+    return new HorseMan;
+  }
+};
+
 
 int main()
 {
-cout << "Hello World!" << endl;
-return 0;
-}
+  InfantryManFactory*infantry_factory= new InfantryManFactory;
+  ArcherManFactory* archers_factory = new ArcherManFactory;
+  HorseManFactory* hourse_man_factory = new HorseManFactory;
+
+  vector<Warrior*> men;
+  men.push_back(infantry_factory->createWarrior());
+  men.push_back(archers_factory->createWarrior());
+  men.push_back(hourse_man_factory->createWarrior());
+ }
