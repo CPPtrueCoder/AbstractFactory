d6cf2a9219e36cebe5e1ed574da2402b2f83ce84
andierodin@gmail.com <andierodin@gmail.com>
2019-04-23 01:18:30 +0300

    ADD file class M1A1 methods GetID , Info;

---
+++ m1a1.h
@@ -0,0 +1,15 @@
+#ifndef M1A1_H
+#define M1A1_H
+#include "tanks.h"
+#include <iostream>
+class M1A1 {
+public:
+  M1A1();
+  void GetID();
+  void Info();
+
+private:
+  int ID;
+};
+
+#endif // M1A1_H
