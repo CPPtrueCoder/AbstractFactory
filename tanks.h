cdc6bcc55c272b07b74d7f13e614518386aeeee0
andierodin@gmail.com <andierodin@gmail.com>
2019-04-23 10:49:36 +0300

    Add
    File interface for working with tanks

---
+++ tanks.h
@@ -0,0 +1,15 @@
+#ifndef TANKS_H
+#define TANKS_H
+#include <iostream>
+using namespace std;
+
+class MainBattleTank {
+public:
+  static int counttanks ;
+  MainBattleTank() {}
+  virtual void Info() = 0;
+  virtual void GetID() = 0;
+  virtual ~MainBattleTank() = default;
+};
+
+#endif
